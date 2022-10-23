0c3be77275ddfcc2f1097ddb268589a40b3cd129
Gyehwan Baek <b99hwan@gmail.com>
2022-10-23 15:12:19 +0900

    feat: 기능,자료구조 구현

---
+++ app/src/main/java/com/example/teamproject1_datastructure/Ingredients.kt
@@ -0,0 +1,88 @@
+package com.example.teamproject1_datastructure
+import java.text.SimpleDateFormat
+import java.util.*
+
+//재료 클래스
+class Ingredients(name:String) {
+    //변수
+    var name : String = name            //재료 이름
+    var picture : String = ""           //재료 사진
+    var num : Int = 0                   // 재료의 개수
+    var expirationDate : String = ""    //재료의 유통기한. 형식: 2022-10-10
+    var expirationDDay : Int = 0        //재료의 남은 유통기한. 현재 날짜 기준.
+    var inputDate : String = ""         //재료를 추가한 날짜. 형식: 2022-10-10
+    var storageMethod : String = ""     //재료 보관 방법
+
+    //보조생성자
+    constructor(name:String, num:Int, expirationDate:String, inputDate:String, storageMethod:String):this(name){
+        this.num = num
+        this.expirationDate = expirationDate
+        this.inputDate = inputDate
+        this.storageMethod = storageMethod
+        this.expirationDDay = calculateDDay(expirationDate)
+    }
+
+    //남은 유통기한을 계산해주는 함수
+    fun calculateDDay(expirationDate: String):Int{
+        var today = Calendar.getInstance()
+        var sf = SimpleDateFormat("yyyy-MM-dd")
+        var date = sf.parse(expirationDate)
+        var DDay = (date.time - today.time.time) / (60*60*24*1000)
+        return DDay.toInt()
+    }
+}
+
+//유통기한 순으로 정렬하는 함수 (합병정렬)
+fun mergeSort(list:LinkedList<Ingredients>, start:Int, end:Int){
+    if (start >= end) return
+    val mid = (start + end) / 2  // 반으로 쪼갬
+    mergeSort(list, start, mid)
+    mergeSort(list, mid + 1, end)
+    merge(list, start, mid, end)  // 분할된 두 리스트을 하나로 정렬하며 합침
+}
+
+fun merge(list:LinkedList<Ingredients>, start: Int, mid: Int, end: Int) {
+    val sortedList:LinkedList<Ingredients> = LinkedList(listOf())  // 정렬된 새로운 리스트
+    var indexA = start  // 왼쪽 배열 인덱스
+    var indexB = mid + 1  // 오른쪽 배열 인덱스
+
+    while (indexA <= mid && indexB <= end) {  // 두 리스트 중 하나라도 모두 소진되면 종료
+        // 둘 중 최솟값을 새로운 리스트에 담아주는 작업
+        if (list[indexA].expirationDDay <= list[indexB].expirationDDay) {
+            sortedList.add(list[indexA])
+            indexA++
+        } else {
+            sortedList.add(list[indexB])
+            indexB++
+        }
+    }
+    if (indexA > mid) {  // 오른쪽 배열 원소가 아직 남았다면
+        for (i in indexB..end) {
+            sortedList.add(list[i])
+        }
+    }
+    if (indexB > end) {  // 왼쪽 배열 원소가 아직 남았다면
+        for (i in indexA..mid) {
+            sortedList.add(list[i])
+        }
+    }
+    for (x in sortedList.indices) {  // 정렬된 부분 대입
+        list[start + x] = sortedList[x]
+    }
+}
+
+fun main(){
+    val egg = Ingredients("달걀", 15, "2022-10-20", "2022-10-10", "냉장보관" )
+    val potato = Ingredients("감자", 10, "2022-11-02", "2022-10-22", "실온보관")
+    val garlic = Ingredients("마늘", 8, "2022-11-15", "2022-10-25", "실온보관")
+    val onion = Ingredients("양파", 3, "2022-11-14", "2022-10-26", "냉장보관")
+    val leek = Ingredients("대파", 2, "2022-11-02", "2022-10-22", "냉장보관")
+
+    var ingredientsList :LinkedList<Ingredients> = LinkedList(listOf(egg,potato,garlic,onion,leek))
+
+    mergeSort(ingredientsList,0,ingredientsList.size-1)
+
+    for(i in ingredientsList.indices){
+        println(ingredientsList[i].name)
+    }
+}
---
+++ app/src/main/java/com/example/teamproject1_datastructure/Recipe.kt
@@ -0,0 +1,92 @@
+package com.example.teamproject1_datastructure
+
+import java.util.LinkedList
+
+//레시피 클래스
+class Recipe(name:String) {
+    //변수
+    var name : String = name         //레시피 이름
+    var picture : String = ""       //레시피 사진 url 스트링
+    var desc : String = ""          //레시피 설명
+    var youtubeLink : String = ""   //레시피 설명 유튜브 링크
+    var category : String = ""      //레시피의 카테고리 ex)한식,중식,양식,,
+    var essentialIngredients = Array<String>(10,{""}) //필수 재료
+    var additionalIngredients = Array<String>(10,{""}) //부가적인 재료
+
+    //보조 생성자
+    constructor(name:String, desc:String, youtubeLink:String, category:String, essentialIngredients: Array<String>, additionalIngredients:Array<String>):this(name) {
+        this.desc = desc
+        this.youtubeLink = youtubeLink
+        this.category = category
+        this.essentialIngredients = essentialIngredients
+        this.additionalIngredients = additionalIngredients
+    }
+}
+
+//사용자가 원하는 카테고리를 입력받고 레시피 목록을 알려주는 함수 (ex. 한식, 중식, 양식, 분식 ..)
+fun findCategory(recipeList:Array<Recipe>, category:String):Array<Recipe?>{
+    var categoryList: Array<Recipe?> = arrayOfNulls<Recipe>(20)
+    var categoryIndex:Int = 0
+    //순차탐색
+    for(index in 0 until recipeList.size){
+        if(recipeList[index].category == category){
+            categoryList[categoryIndex] = recipeList[index]
+            categoryIndex++
+        }
+    }
+    return categoryList
+}
+
+//레시피 검색 함수
+fun findRecipe(recipeList:Array<Recipe>, ingredientList:LinkedList<Ingredients>):Array<Recipe?>{
+    var possibleRecipe: Array<Recipe?> = arrayOfNulls<Recipe>(20) //가능한 레시피 목록
+    var recipeIndex:Int = 0
+    var possible:Boolean = false
+    for(i in 0 until recipeList.size){
+        possible = false
+        for(j in 0 until recipeList[i].essentialIngredients.size){
+            for(k in 0 until ingredientList.size){
+                if(ingredientList[k].name == recipeList[i].essentialIngredients[j]){
+                    //여기 들어오면 필수재료 찾은것.
+                    possible = true
+                    break
+                }
+            }
+            if(possible == false){
+                break
+            }
+        }
+        if(possible == true){
+            //여기서 가능한 레시피 배열에 추가해주면 된다.
+            possibleRecipe[recipeIndex] = recipeList[i]
+            recipeIndex++
+        }
+    }
+    for(i in possibleRecipe.indices){
+        println(possibleRecipe[i]?.name)
+    }
+
+    return possibleRecipe
+}
+
+fun main(){
+    // 레시피 추가
+    val kimchiBog = Recipe("김치볶음밥", "김치와 밥을 넣고 볶는다", "www.youtube.com", "한식", arrayOf("김치", "달걀"), arrayOf("대파", "양파", "마늘"))
+    val gimbap = Recipe("김밥", "김에 재료를 넣고 말아준다", "www.youtube.com_gimbap", "한식", arrayOf("김","밥","단무지"),arrayOf("달걀","시금치"))
+    val pasta = Recipe("파스타", "면을 삶는다", "www.파스타.com","양식", arrayOf("면","소스"), arrayOf("파슬리"))
+    val simple = Recipe("기본", "ㅋㅋ", "www", "한식",  arrayOf("달걀","감자"), arrayOf("마늘"))
+
+    var recipeList:Array<Recipe> = arrayOf(kimchiBog,gimbap,pasta,simple)
+
+    // 재료 추가
+    val egg = Ingredients("달걀", 15, "2022-10-20", "2022-10-10", "냉장보관" )
+    val potato = Ingredients("감자", 10, "2022-11-02", "2022-10-22", "실온보관")
+    val garlic = Ingredients("마늘", 8, "2022-11-15", "2022-10-25", "실온보관")
+    val onion = Ingredients("양파", 3, "2022-11-14", "2022-10-26", "냉장보관")
+    val leek = Ingredients("대파", 2, "2022-11-02", "2022-10-22", "냉장보관")
+    val kimchi = Ingredients("김치", 1, "2023-03-03", "2022-10-12", "냉장보관")
+
+    var ingredientsList :LinkedList<Ingredients> = LinkedList(listOf(egg,potato,garlic,onion,leek,kimchi))
+
+    println(findRecipe(recipeList,ingredientsList))
+}
